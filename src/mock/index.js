a1e80a3244f5e089e893b3825ef3633e9bc39163
xiahongxiu <790648858@qq.com>
2020-02-24 14:05:58 +0800

    删除

--- src/mock/index.js
+++
@@ -1,8 +0,0 @@
-import { mock } from "@/util/util";
-// result接口mock
-import "./result";
-
-// 登录接口
-mock("epidemic/entry/login", {
-  code: 10000
-});
--- src/plugins/iview.js
+++
@@ -1,8 +0,0 @@
-import Vue from "vue";
-import ViewUI from "view-design";
-import zn from "view-design/dist/locale/zh-CN";
-
-Vue.use(ViewUI, { zn });
-
-import "view-design/dist/styles/iview.css";
-import "../styles/index.less";
